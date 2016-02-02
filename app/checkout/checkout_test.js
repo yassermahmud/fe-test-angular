6586f2a9d8160547af2f57a70ae74d7c1e150565
Merrick Fox <merrick.fox@gmail.com>
2016-02-02 13:22:10 +0000

    checkout test file

--- app/checkout/checkout_test.js
+++ app/checkout/checkout_test.js
@@ -2,15 +2,11 @@
 
 describe('myApp.view1 module', function() {
 
-  beforeEach(module('myApp.view1'));
+  beforeEach(module('myApp.chekout'));
 
-  describe('view1 controller', function(){
+  describe('checkout controller', function(){
 
-    it('should ....', inject(function($controller) {
-      //spec body
-      var view1Ctrl = $controller('View1Ctrl');
-      expect(view1Ctrl).toBeDefined();
-    }));
+    //Your test implementation can go here
 
   });
 });
